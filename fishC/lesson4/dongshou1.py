8176e060a2537942401dd5893ddc2c0958660276
原志强 <zhiqiang.yuan@cloudcross.com>
2016-04-09 17:17:05 +0800

    第四课的练习

---
+++ fishC/lesson4/dongshou1.py
@@ -0,0 +1,6 @@
+lens = input('请输入一个整数：')
+i = 1
+lens = int(lens)
+while i<=lens:
+    print(i)
+    i += 1 
---
+++ fishC/lesson4/dongshou2.py
@@ -0,0 +1,6 @@
+lens = input('请输入一个整数：')
+lens = int(lens)
+i = 8
+while(i >= 1):
+    print(" "*i+"*"*i)
+    i -= 1
